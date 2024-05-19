b76920469ca67c96d26cb5ba2030199886d81c66
Volodymyr892 <sevcukvolodimir896@gmail.com>
2024-05-19 11:38:13 +0300

    Update render-functions.js

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,10 +1,10 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-let lightbox;
 
 export const createMarkupItem = images => {
-    return images.map(
+   
+     images.map(
         ({
             webformatURL,
             largeImageURL,
@@ -13,41 +13,41 @@ export const createMarkupItem = images => {
             views,
             comments,
             downloads,
-        }) => { return`
+        }) => { `
             <li class="item-list">
-            <a href="${largeImageURL}" class="item-list-link">
+        <a href="${largeImageURL}" class="item-list-link">
             <img class="item-list-img"  height="152"
             width="300" src="${webformatURL}" alt="${tags}">
         </a>
         <div class='markup-info'>
-        <div class="item-list-info-text">
-        <h3 class="item-list-title">Likes</h3>
-        <p class="item-list-text">${likes}</p>
-        </div>
-        <div class="item-list-info-text">
-        <h3 class="item-list-title">Views</h3>
-        <p class="item-list-text">${views}</p>
-        </div>
-        <div class="item-list-info-text">
-        <h3 class="item-list-title">Comments</h3>
-        <p class="item-list-text">${comments}</p>
-        </div>
             <div class="item-list-info-text">
-            <h3 class="item-list-title">Downloads</h3>
-            <p class="item-list-text">${downloads}</p>
+                <h3 class="item-list-title">Likes</h3>
+                    <p class="item-list-text">${likes}</p>
+            </div>
+            <div class="item-list-info-text">
+                <h3 class="item-list-title">Views</h3>
+                    <p class="item-list-text">${views}</p>
+            </div>
+            <div class="item-list-info-text">
+                <h3 class="item-list-title">Comments</h3>
+                    <p class="item-list-text">${comments}</p>
+            </div>
+            <div class="item-list-info-text">
+                <h3 class="item-list-title">Downloads</h3>
+                <p class="item-list-text">${downloads}</p>
             </div>
             </div>
             </li>
             `
-           } ).join("");
+           }).join("");
            
-        }
-        lightbox = new SimpleLightbox('.item-list-link', {
-            captionsData: 'alt', 
-            captionDelay: 250 ,
-            overlayOpacity: 0.8,
-        });
+           lightbox = new SimpleLightbox('item-list a', {
+               captionsData: 'alt', 
+               captionDelay: 250 ,
+               overlayOpacity: 0.8,
+           }).refresh();
+        return lightbox;
+           
+};
         
-        lightbox.refresh();
         
-    console.log("🚀 ~ createMarkupItem ~ createMarkupItem:", createMarkupItem) 
