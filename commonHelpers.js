5ffa338739c5e21b13b718cb6e5f01c64e7539eb
Volodymyr892 <114510313+Volodymyr892@users.noreply.github.com>
2024-05-19 08:15:56 +0000

    Deploying to gh-pages from @ Volodymyr892/goit-js-hw-11@cf1dde3833a88b0b70d8a0ee0c2aa619246a1099 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,8 +1,8 @@
-import{i as n}from"./assets/vendor-482df00d.js";(function(){const i=document.createElement("link").relList;if(i&&i.supports&&i.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))l(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&l(o)}).observe(document,{childList:!0,subtree:!0});function s(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function l(e){if(e.ep)return;e.ep=!0;const t=s(e);fetch(e.href,t)}})();const d="43948978-860dfecfaa788bc4b5c828749",f="https://pixabay.com/api/",h=(r="pug")=>{const i=new URLSearchParams({key:d,query:r,image_type:"photo",orientation:"horizontal",safesearch:!0});return fetch(`${f}?${i}`).then(s=>{if(!s.ok)throw new Error(s.statusText);return s.json()})},m=r=>r.map(({webformatURL:i,largeImageURL:s,tags:l,likes:e,views:t,comments:o,downloads:u})=>`
+import{S as h,i as c}from"./assets/vendor-0fc460d7.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))l(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&l(o)}).observe(document,{childList:!0,subtree:!0});function i(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function l(e){if(e.ep)return;e.ep=!0;const t=i(e);fetch(e.href,t)}})();const f="43948978-860dfecfaa788bc4b5c828749",p="https://pixabay.com/api/",y=(r="pug")=>{const s=new URLSearchParams({key:f,query:r,image_type:"photo",orientation:"horizontal",safesearch:!0});return fetch(`${p}?${s}`).then(i=>{if(!i.ok)throw new Error(i.statusText);return i.json()})};let m;const u=r=>r.map(({webformatURL:s,largeImageURL:i,tags:l,likes:e,views:t,comments:o,downloads:d})=>`
             <li class="item-list">
-            <a href="${s}" class="item-list-link">
+            <a href="${i}" class="item-list-link">
             <img class="item-list-img"  height="152"
-            width="300" src="${i}" alt="${l}">
+            width="300" src="${s}" alt="${l}">
         </a>
         <div class='markup-info'>
         <div class="item-list-info-text">
@@ -19,9 +19,9 @@ import{i as n}from"./assets/vendor-482df00d.js";(function(){const i=document.cre
         </div>
             <div class="item-list-info-text">
             <h3 class="item-list-title">Downloads</h3>
-            <p class="item-list-text">${u}</p>
+            <p class="item-list-text">${d}</p>
             </div>
             </div>
             </li>
-            `).join("");console.log("🚀 ~ createMarkupItem ~ createMarkupItem:",m);const p=document.querySelector(".js-search-form"),c=document.querySelector(".js-gallery"),a=document.querySelector(".js-loader");function y(r){r.preventDefault();const i=r.target.elements.searchKeyword.value.trim();if(i===""){c.innerHTML="",r.target.reset(),n.show({message:"Input field can not be empty",position:"topRight",timeout:2e3,color:"red"});return}c.innerHTML="",a.classList.remove("is-hidden"),h(i).then(s=>{s.total===0&&n.show({message:"Sorry, there are no images for this query",position:"topRight",timeout:2e3,color:"red"}),c.innerHTML=m(s.hits)}).catch(s=>console.log(s)).finally(()=>{r.target.reset(),a.classList.add("is-hidden")})}p.addEventListener("submit",y);
+            `).join("");m=new h(".item-list-link",{captionsData:"alt",captionDelay:250,overlayOpacity:.8});m.refresh();console.log("🚀 ~ createMarkupItem ~ createMarkupItem:",u);const g=document.querySelector(".js-search-form"),a=document.querySelector(".js-gallery"),n=document.querySelector(".js-loader");function L(r){r.preventDefault();const s=r.target.elements.searchKeyword.value.trim();if(s===""){a.innerHTML="",r.target.reset(),c.show({message:"Input field can not be empty",position:"topRight",timeout:2e3,color:"red"});return}a.innerHTML="",n.classList.remove("is-hidden"),y(s).then(i=>{i.total===0&&c.show({message:"Sorry, there are no images for this query",position:"topRight",timeout:2e3,color:"red"}),a.innerHTML=u(i.hits)}).catch(i=>console.log(i)).finally(()=>{r.target.reset(),n.classList.add("is-hidden")})}g.addEventListener("submit",L);
 //# sourceMappingURL=commonHelpers.js.map
